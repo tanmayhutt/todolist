c7bb9ed8b86c00fa9349778ef0f7f538308638e4
tanmayhutt <tiwaritanmay1021@gmail.com>
2025-06-09 20:23:14 +0530

    vercel update

--- api/index.py
+++
@@ -1,4 +0,0 @@
-from todolist.wsgi import application
-
-def handler(request, context):
-    return application
--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ws(eot4vrnivbkn=r%fgazb)#kaahrz+ekkiy4^mirl%ux_1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
